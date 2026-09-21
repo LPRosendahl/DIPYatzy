dfa3546188329ebb420786bf214a17ac0650d570
RedNickster <n.stenlokke@gmail.com>
2026-09-21 11:18:17 +0200

    Store ændringer i gui
    Der er lavet store ændringer i gui.js. Der er lavet funktioner til at opdatere/rendere både dice området og alle vores værdier. Der er dertil gjordt klar til at vi kan lave videre på score area.
    
    Dertil dumme filer slettet
    
    Og til sidst er rollRandomDIce() lavet om så denne returnere en liste istedet for bare et tal. Den opdatere også dice variablen inde i logik.js

--- client/gui.js
+++ client/gui.js
@@ -1,8 +1,42 @@
-const leftColumn = document.querySelector("#left-column");
-const rightColumn = document.querySelector("#right-column");
-const rightPanel = document.querySelector("#right-panel");
-const gamePanel = document.querySelector("#game-panel");
-const dice = document.querySelectorAll(".dice-list img");
-const rollButton = document.querySelector("#roll-button");
+import { rollRandomDice } from "../server/logik.js";
 
-console.log({ leftColumn, rightColumn, rightPanel, gamePanel, dice, rollButton });
+const diceImages = Array.from(document.querySelectorAll(".dice-list img"));
+const rollButton = document.getElementById("roll-button");
+const scoreInputs = Array.from(document.querySelectorAll(".score-list input"));
+
+const dieImageNames = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six"
+];
+
+function renderDice(diceValues) {
+    diceValues.forEach((value, index) => {
+        const image = diceImages[index];
+        if (!image) return;
+
+        image.src = `images/${dieImageNames[value - 1]}.png`;
+        image.alt = `Dice ${value}`;
+    });
+}
+
+function renderScoreArea(scores) {
+    if (!scores || scores.length === 0) return;
+
+    scoreInputs.forEach((input, index) => {
+        input.value = scores[index] ?? 0;
+    });
+}
+
+if (rollButton) {
+    rollButton.addEventListener("click", () => {
+        const diceValues = rollRandomDice();
+        renderDice(diceValues);
+
+        // Denne linje skal erstattes af jeres logik, når I har score-beregningen klar.
+        renderScoreArea([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    });
+}
